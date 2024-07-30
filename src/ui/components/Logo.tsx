afa283f76dc7deb9eb3c8e6d90472ce2aacfc925
ComputaGeek <81460315+AmonKissi@users.noreply.github.com>
2024-07-30 18:02:10 +0000

    fixing logo

--- src/ui/components/Logo.tsx
+++ src/ui/components/Logo.tsx
@@ -8,19 +8,25 @@ const logoUrl = "https://image.noelshack.com/fichiers/2024/31/2/1722356811-img-4
 export const Logo = () => {
     const pathname = usePathname();
 
+    const logoStyle = {
+        maxWidth: '35%',
+        height: 'auto',
+    };
+
     if (pathname === "/") {
         return (
             <h1 className="flex items-center font-bold" aria-label="homepage">
-                <img src={logoUrl} alt="Company Logo" />
+                <img src={logoUrl} alt="Company Logo" style={logoStyle} />
             </h1>
         );
     }
     return (
         <div className="flex items-center font-bold">
             <LinkWithChannel aria-label="homepage" href="/">
-                <img src={logoUrl} alt="Company Logo" />
+                <img src={logoUrl} alt="Company Logo" style={logoStyle} />
             </LinkWithChannel>
         </div>
     );
 };
+
 
