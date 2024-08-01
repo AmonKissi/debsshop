dd095ef1929c47f6c5368f20a8d3f5a1ac55d14b
ComputaGeek <81460315+AmonKissi@users.noreply.github.com>
2024-08-01 08:41:57 +0000

    fixing error

--- src/commande/index.tsx
+++ src/commande/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import Script from 'next/script';
 
-const FullScreenFormEmbed: React.FC = () => {
+export const FullScreenFormEmbed: React.FC = () => {
   return (
     <div style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0 }}>
       <div
@@ -9,9 +10,10 @@ const FullScreenFormEmbed: React.FC = () => {
         style={{ width: '100%', height: '100%' }}
         data-fillout-inherit-parameters
       ></div>
-      <script src="https://server.fillout.com/embed/v1/" />
+      <Script
+        src="https://server.fillout.com/embed/v1/"
+        strategy="afterInteractive"
+      />
     </div>
   );
 };
-
-export default FullScreenFormEmbed;
